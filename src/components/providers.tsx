72a89c0061159c97e22833bb8764b746401d322d
smedeN93 <nicklasmc93@gmail.com>
2024-06-25 18:36:26 +0200

    asd

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -28,7 +28,7 @@ export const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: "/api/trpc",
         }),
       ],
     }),
